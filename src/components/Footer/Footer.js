041b4f63547c4a825a3bc7068cfbe4b23a7ea7b4
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-04 00:54:36 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer'>
+            <p>Copyright &copy; 2022 Hyper Sneakers</p>
         </div>
     );
 };
