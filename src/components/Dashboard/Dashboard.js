b934ae521a63e54e5f98ba2431b453a9bef89660
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-04 14:22:26 +0600

    dashboard page added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,21 +1,127 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Area,
+    AreaChart,
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    Pie,
+    PieChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
 import './Dashboard.css';
 
 const Dashboard = () => {
     const [chart, setChart] = useState([]);
-    
+
     useEffect(() => {
         fetch(`chart.json`)
-        .then(res => res.json())
-        .then(data => setChart(data))
-    }, [])
+            .then((res) => res.json())
+            .then((data) => setChart(data));
+    }, []);
 
-    console.log(chart)
+    console.log(chart);
 
     return (
         <div className="dashboard-container">
             <div className="container">
-                <h1>Dashboard</h1>
+                <div className="chart-item">
+                    <h3>Year Wise Sell</h3>
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={chart}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="year" />
+                        <YAxis />
+                        <Tooltip />
+                        <Line
+                            type="monotone"
+                            dataKey="sell"
+                            stroke="#fbd602"
+                            activeDot={{ r: 8 }}
+                        />
+                    </LineChart>
+                </div>
+
+                <div className="chart-item">
+                    <h3>Year Wise Investment</h3>
+                    <AreaChart
+                        width={500}
+                        height={300}
+                        data={chart}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="sell" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area
+                            type="monotone"
+                            dataKey="investment"
+                            stroke="#fbd602"
+                            fill="#fbd602"
+                        />
+                    </AreaChart>
+                </div>
+
+                <div className="chart-item">
+                    <h3>Investment</h3>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chart}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="year" />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="investment" fill="#fbd602" />
+                    </BarChart>
+                </div>
+
+                <div className="chart-item">
+                    <h3>Year wise sell</h3>
+                    <ResponsiveContainer width="100%" height={300}>
+                        <PieChart
+                            width={400}
+                            height={300}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <Pie data={chart} dataKey="sell" fill="#fbd602" />
+                            <Tooltip />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         </div>
     );
